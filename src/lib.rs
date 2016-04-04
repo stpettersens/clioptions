140d7408d8cd757684575c49fea3d8e1684d21b0
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-04-04 13:48:31 +0100

    fix spacing.

--- src/lib.rs
+++ src/lib.rs
@@ -6,45 +6,45 @@
 use std::env;
 
 pub struct CliOptions {
-	program: String,
+    program: String,
 }
 
 impl CliOptions {
 
-	/// Create new instance of CliOptions.
-	pub fn new(program: &str) -> CliOptions {
-		CliOptions {
-			program: program.to_string(),
-		}
-	}
+    /// Create new instance of CliOptions.
+    pub fn new(program: &str) -> CliOptions {
+        CliOptions {
+            program: program.to_string(),
+        }
+    }
 
-	/// Get any command line arguments.
-	pub fn get_args(&self) -> Vec<String> {
+    /// Get any command line arguments.
+    pub fn get_args(&self) -> Vec<String> {
         env::args().collect()
-	}
+    }
     
-    	/// Get number fo command line arguments.
+        /// Get number fo command line arguments.
     pub fn get_num(&self) -> usize {
         self.get_args().clone().len()
     }
 
-	/// Get next argument.
+    /// Get next argument.
     pub fn next_argument(&self, i: usize) -> String {
-	   let a = env::args().nth(i + 1);
-	   let arg = match a {
+       let a = env::args().nth(i + 1);
+       let arg = match a {
             Some(a) => a,
             None => String::new()
        };
        arg
-	}
+    }
 
     /// Get program name from first argument.
     pub fn get_program(&self) -> String {
-    	let a = env::args().nth(0);
-    	let arg = match a {
-    		Some(a) => a,
-    		None => self.program.clone()
-    	};
+        let a = env::args().nth(0);
+        let arg = match a {
+            Some(a) => a,
+            None => self.program.clone()
+        };
         arg
     }
 }
