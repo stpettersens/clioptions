8eace6d6056a5479595694e7e5812955ef397e88
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-10-06 18:43:13 +0100

    commit documentation.

---
+++ doc/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["clioptions"] = {"doc":"A very thin wrapper for command line arguments in Rust.","items":[[3,"CliOptions","clioptions","",null,null],[11,"new","","Create new instance of CliOptions.",0,{"inputs":[{"name":"str"}],"output":{"name":"clioptions"}}],[11,"get_args","","Get any command line arguments.",0,null],[11,"get_num","","Get number fo command line arguments.",0,null],[11,"next_argument","","Get next argument.",0,null],[11,"get_program","","Get program name from first argument.",0,null]],"paths":[[3,"CliOptions"]]};
+initSearch(searchIndex);
